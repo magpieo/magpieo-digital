7cde8fc23af7198d54b1fe01f5900adcee0931a5
MohammadJewelRana <js.rana0326@gmail.com>
2025-09-22 17:27:50 +0600

    project setup

---
+++ src/app/components/shared/Loading.tsx
@@ -0,0 +1,16 @@
+ 
+ 
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="bg-black/10  h-screen fixed inset-0 z-[999] backdrop-blur-md flex justify-center items-center">
+      {/* <Spinner size="lg" /> */}
+
+      {/* <Spinner color="warning" label="Loading"   /> */}
+      <p>Loading</p>
+    </div>
+  );
+};
+
+export default Loading;
---
+++ src/app/config/envConfig.ts
@@ -0,0 +1,5 @@
+const envConfig = {
+  baseApi: process.env.NEXT_PUBLIC_BASE_API, // Correct way to access env variables in Next.js
+};
+
+export default envConfig;
