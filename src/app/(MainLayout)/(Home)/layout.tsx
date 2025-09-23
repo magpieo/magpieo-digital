fe2d0695be6966557fb58c1f5001301d976450b6
MohammadJewelRana <js.rana0326@gmail.com>
2025-09-23 12:44:26 +0600

    basic layout of navbar

--- src/app/(MainLayout)/(Home)/layout.tsx
+++ src/app/(MainLayout)/(Home)/layout.tsx
@@ -1,5 +1,14 @@
+import Navbar from "@/app/components/shared/Navbar";
+
+ 
+
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+  return (
+    <div>
+ <Navbar/>
+      {children}
+    </div>
+  );
 };
 
 export default layout;
