8675db134784ea82bf026c43a7ef667c2b28e407
MohammadJewelRana <js.rana0326@gmail.com>
2025-09-29 16:23:42 +0600

    basic layout ready for footer

--- src/app/(MainLayout)/(Home)/_components/Home.tsx
+++ src/app/(MainLayout)/(Home)/_components/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import OurService from "./Service";
 import HeroSection from "./HeroSection";
+import Footer from "@/app/components/shared/Footer";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <Footer/>
       <div className="px-4">
         <div className="max-w-6xl mx-auto ">
           <OurService />
