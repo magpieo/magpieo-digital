295432a7f3a714db9bd4d4d1b27ad65ff3d40115
rakibaakter <rakibaakter635@gmail.com>
2025-10-21 17:39:26 +0600

    working on new hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
-        paragraph: ['12px', '1.5rem'], // default
-        paragraphLg: ['14px', '1.5rem'], // large screens
-        heading: ['32px', '1.2'], // default
-        headingLg: ['40px', '1.2'], // large
-        subheading: ['22px', '1.3'],
-        subheadingLg: ['28px', '1.3'],
+        paragraph: ["12px", "1.5rem"], // default
+        paragraphLg: ["14px", "1.5rem"], // large screens
+        heading: ["32px", "1.2"], // default
+        headingLg: ["40px", "1.2"], // large
+        subheading: ["22px", "1.3"],
+        subheadingLg: ["28px", "1.3"],
       },
     },
   },
